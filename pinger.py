12552e08292e2339fe51b3556e646d4848b2b939
roopesh90 <roopesh90nair@live.com>
2015-10-30 19:17:34 +0530

    exponential backoff

--- pinger.py
+++ pinger.py
@@ -1,1 +1,10 @@
 #! /usr/bin/python3
+
+'''
+exponential backoff states that:
+    Given a uniform distribution of backoff times, the expected backoff time is the mean of the possibilities.
+    That is, after c collisions, the number of backoff slots is in 
+        [0, 1, ..., N], where N = 2c - 1 
+    and the expected backoff time (in slots) is:
+    (i/(N+1))*(n∑i,i=0), where (n∑i,i=0) = ((n*(n+1))/2)
+'''
